7da48f75a03dae340a3040cf7db08420642e6d57
최상배 <sbchoi129@choesangbaeui-MacBookPro.local>
2024-11-29 23:45:22 +0900

    docs : Add comments in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+# City Dictionary
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
@@ -233,6 +234,7 @@ def main():
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
+	# It shows raw data
         display_raw_data(df)
 
         if len(df) == 0:
